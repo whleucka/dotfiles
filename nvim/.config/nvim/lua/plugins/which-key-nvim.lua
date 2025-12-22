107b91efe8069f4a666a706c1813398f313e177b
William Hleucka <william.hleucka@gmail.com>
2025-12-22 08:37:06 -0700

    fix: update plugin name

--- nvim/.config/nvim/lua/plugins/which-key-nvim.lua
+++ nvim/.config/nvim/lua/plugins/which-key-nvim.lua
@@ -1,7 +1,6 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
-  main = "which-key",
   config = function()
     local keymap = require("core.keymap")
     require("which-key").add(keymap)
