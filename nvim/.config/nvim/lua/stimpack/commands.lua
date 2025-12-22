f471fe330ce41c4a5fbd18db681f358898f27113
William Hleucka <william.hleucka@gmail.com>
2025-12-22 08:45:48 -0700

    wip: StimStatus

--- nvim/.config/nvim/lua/stimpack/commands.lua
+++ nvim/.config/nvim/lua/stimpack/commands.lua
@@ -31,6 +31,28 @@ function M.setup(stimpack)
   vim.api.nvim_create_user_command("StimNuke", function()
     stimpack.nuke()
   end, {})
+  -- cmd lists all plugins on disk
+  vim.api.nvim_create_user_command("StimStatus", function()
+    local spec_util = require("stimpack.spec")
+    local lines = {}
+    for _, spec in ipairs(stimpack.config.specs) do
+      local name = spec.name or spec_util.get_name(spec_util.get_source(spec))
+      local rev
+      if spec.dir then
+        rev = "local"
+      else
+        local ok, info = pcall(vim.pack.get, { name }, { info = true })
+        if ok and info and info[1] then
+          rev = (info[1].rev or "N/A"):sub(1, 7)
+        else
+          rev = "unknown"
+        end
+      end
+      table.insert(lines, string.format("%s (%s)", name, rev))
+    end
+    vim.notify(table.concat(lines, "\n"), vim.log.levels.INFO)
+    vim.notify("Stimpack does not yet track plugin load times.", vim.log.levels.INFO)
+  end, {})
 end
 
 return M
