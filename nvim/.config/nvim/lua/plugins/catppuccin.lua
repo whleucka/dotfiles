c4002ef626508ae469cc18e1277e70c4942d0dbd
William Hleucka <william.hleucka@gmail.com>
2025-12-24 11:13:18 -0700

    fix: update a bunch of stuff

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -7,12 +7,12 @@ return {
     local config = require("config.catppuccin")
     require("catppuccin").setup(config)
     vim.cmd.colorscheme "catppuccin"
-    vim.cmd [[
-        hi Normal guibg=NONE ctermbg=NONE
-        hi NormalNC guibg=NONE ctermbg=NONE
-        hi SignColumn guibg=NONE ctermbg=NONE
-        hi LineNr guibg=NONE ctermbg=NONE
-        hi EndOfBuffer guibg=NONE ctermbg=NONE
-      ]]
+    --vim.cmd [[
+    --    hi Normal guibg=NONE ctermbg=NONE
+    --    hi NormalNC guibg=NONE ctermbg=NONE
+    --    hi SignColumn guibg=NONE ctermbg=NONE
+    --    hi LineNr guibg=NONE ctermbg=NONE
+    --    hi EndOfBuffer guibg=NONE ctermbg=NONE
+    --  ]]
   end
 }
