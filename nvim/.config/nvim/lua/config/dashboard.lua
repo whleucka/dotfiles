1edd2158c651f02b1336e3b02c829a5d353d52ce
William Hleucka <william.hleucka@gmail.com>
2025-12-29 12:44:47 -0700

    fix: dash config

--- nvim/.config/nvim/lua/config/dashboard.lua
+++ nvim/.config/nvim/lua/config/dashboard.lua
@@ -1,79 +1,77 @@
-return function()
-  return {
-    theme = "hyper",
-    config = {
-      project = {
-        enable = true,
-        action = function(path)
-          -- use fzf-lua instead of telescope
-          require("fzf-lua").files({ cwd = path })
+return {
+  theme = "hyper",
+  config = {
+    project = {
+      enable = true,
+      action = function(path)
+        -- use fzf-lua instead of telescope
+        require("fzf-lua").files({ cwd = path })
+      end,
+    },
+    week_header = {
+      enable = true,
+    },
+    packages = { enable = false },
+    shortcut = {
+      {
+        desc = "Mount",
+        group = "Label",
+        action = ":SSHConnect",
+        key = "m",
+      },
+      {
+        desc = "New",
+        group = "Label",
+        action = ":enew",
+        key = "n",
+      },
+      {
+        desc = "Git",
+        group = "Label",
+        action = ":Neogit",
+        key = "g",
+      },
+      {
+        desc = "Oil",
+        group = "Label",
+        action = ":Oil",
+        key = "o",
+      },
+      {
+        desc = "Files",
+        group = "Label",
+        action = "FzfLua files",
+        key = "f",
+      },
+      {
+        desc = "Sync",
+        group = "Label",
+        action = ":StimSync",
+        key = "s",
+      },
+      {
+        desc = "Config",
+        group = "Label",
+        action = function()
+          require("fzf-lua").files({
+            cwd = vim.fn.stdpath("config"),
+          })
         end,
+        key = "c",
       },
-      week_header = {
-        enable = true,
-      },
-      packages = { enable = false },
-      shortcut = {
-        {
-          desc = "Mount",
-          group = "Label",
-          action = ":SSHConnect",
-          key = "m",
-        },
-        {
-          desc = "New",
-          group = "Label",
-          action = ":enew",
-          key = "n",
-        },
-        {
-          desc = "Git",
-          group = "Label",
-          action = ":Neogit",
-          key = "g",
-        },
-        {
-          desc = "Oil",
-          group = "Label",
-          action = ":Oil",
-          key = "o",
-        },
-        {
-          desc = "Files",
-          group = "Label",
-          action = "FzfLua files",
-          key = "f",
-        },
-        {
-          desc = "Sync",
-          group = "Label",
-          action = ":StimSync",
-          key = "s",
-        },
-        {
-          desc = "Config",
-          group = "Label",
-          action = function()
-            require("fzf-lua").files({
-              cwd = vim.fn.stdpath("config"),
-            })
-          end,
-          key = "c",
-        },
-        {
-          desc = "Quit",
-          group = "Number",
-          action = function()
-            vim.cmd [[quit]]
-          end,
-          key = "q",
-        },
-      },
-      footer = {
-        "",
-        "With great power comes great responsibility",
-        "",
+      {
+        desc = "Quit",
+        group = "Number",
+        action = function()
+          vim.cmd [[quit]]
+        end,
+        key = "q",
       },
     },
-  }
-end
+    footer = {
+      "",
+      "With great power comes great responsibility",
+      "",
+    },
+  },
+}
--- nvim/.config/nvim/lua/plugins/dashboard-nvim.lua
+++ nvim/.config/nvim/lua/plugins/dashboard-nvim.lua
@@ -3,7 +3,7 @@ return {
   priority = 0, -- load this plugin last to get accurate startup time
   event = "VimEnter",
   config = function()
-    local config = require("config.dashboard")()
+    local config = require("config.dashboard")
     require("dashboard").setup(config)
   end,
   keys = {
