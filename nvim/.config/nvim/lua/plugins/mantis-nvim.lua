8fc7b3f916ecdc70f3d117ca8b7ac8dc8addc132
William Hleucka <william.hleucka@gmail.com>
2026-01-07 10:05:05 -0700

    fix: update a few things

--- nvim/.config/nvim/lua/plugins/mantis-nvim.lua
+++ nvim/.config/nvim/lua/plugins/mantis-nvim.lua
@@ -1,32 +1,23 @@
 return {
-  enabled = false,
-  dir = "/home/whleucka/Projects/mantis.nvim",
-  name = "mantis.nvim",
+  enabled = true,
+  dir = "/home/whleucka/Projects/mantis-nvim",
+  event = "VimEnter",
+  keys = {
+    { "<leader>e", ":Mantis<cr>", desc = "Mantis" },
+  },
   config = function()
-    require("mantis.config").setup({
-      url = "https://mantis.williamhleucka.com",
-      token = "gpYB3wZDDWxm0i0HvMHON2mfMkmq8cSh"
-    })
-
-
-    -- Register the MantisGetIssue command
-    vim.api.nvim_create_user_command('MantisGetIssue', function(args)
-      local mantis = require("mantis")
-      local id = args.fargs[1]
-      if not id then
-        vim.notify('MantisGetIssue: Please provide an issue ID.', vim.log.levels.ERROR)
-        return
-      end
-      local client = mantis.new() -- Create a new client instance
-      local issue = client:get_issue(id)
-      if issue then
-        vim.pretty_print(issue)
-      else
-        vim.notify('MantisGetIssue: Failed to retrieve issue ' .. id, vim.log.levels.ERROR)
-      end
-    end, {
-      nargs = 1,
-      desc = 'Get a Mantis issue by ID',
+    require("mantis").setup({
+      debug = false,
+      hosts = {
+        williamhleucka = {
+          url = "https://mantis.williamhleucka.com",
+          token = "gpYB3wZDDWxm0i0HvMHON2mfMkmq8cSh"
+        },
+        chainlogic = {
+          url = "https://mantis.chainlogic.it",
+          token = "_RNzBd8Y2nDXS2_wR-rghoEfbfJDQ_J2"
+        },
+      }
     })
   end
 }
