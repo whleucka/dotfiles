c2016c805b33928b32ef3e18d6f95ce554aa6c8e
William Hleucka <william.hleucka@gmail.com>
2026-01-07 10:05:05 -0700

    fix: update a few things

---
+++ nvim/.config/nvim/lua/plugins/mantis-nvim.lua
@@ -0,0 +1,23 @@
+return {
+  enabled = true,
+  dir = "/home/whleucka/Projects/mantis-nvim",
+  event = "VimEnter",
+  keys = {
+    { "<leader>e", ":Mantis<cr>", desc = "Mantis" },
+  },
+  config = function()
+    require("mantis").setup({
+      debug = false,
+      hosts = {
+        williamhleucka = {
+          url = "https://mantis.williamhleucka.com",
+          token = "gpYB3wZDDWxm0i0HvMHON2mfMkmq8cSh"
+        },
+        chainlogic = {
+          url = "https://mantis.chainlogic.it",
+          token = "_RNzBd8Y2nDXS2_wR-rghoEfbfJDQ_J2"
+        },
+      }
+    })
+  end
+}
