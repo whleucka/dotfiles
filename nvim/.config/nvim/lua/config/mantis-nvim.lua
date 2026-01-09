03e94f70483a15bfc7afff7f7af2f962f14568bc
William Hleucka <william.hleucka@gmail.com>
2026-01-09 09:02:01 -0700

    fix: add mantis-nvim config

---
+++ nvim/.config/nvim/lua/config/mantis-nvim.lua
@@ -0,0 +1,16 @@
+return {
+  debug = false,
+  hosts = {
+    williamhleucka = {
+      url = "https://mantis.williamhleucka.com",
+      token = "gpYB3wZDDWxm0i0HvMHON2mfMkmq8cSh"
+    },
+    -- chainlogic = {
+    --   url = "https://mantis.chainlogic.it",
+    --   token = "_RNzBd8Y2nDXS2_wR-rghoEfbfJDQ_J2"
+    -- },
+  },
+  view_issues = {
+    page_size = 20,
+  }
+}
