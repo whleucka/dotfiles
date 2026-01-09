23bcb32a13330712b5d5711a9c54f7d086db3f84
William Hleucka <william.hleucka@gmail.com>
2026-01-09 09:04:30 -0700

    fix: rebase

--- nvim/.config/nvim/lua/plugins/mantis-nvim.lua
+++ nvim/.config/nvim/lua/plugins/mantis-nvim.lua
@@ -1,6 +1,7 @@
 return {
   enabled = true,
   dir = "/home/whleucka/Projects/mantis-nvim",
+<<<<<<< HEAD
   name = "mantis-nvim",
   event = "VimEnter",
   keys = {
@@ -11,4 +12,25 @@ return {
     "MunifTanjim/nui.nvim"
   },
   opts = require("config.mantis-nvim")
+=======
+  event = "VimEnter",
+  keys = {
+    { "<leader>e", ":Mantis<cr>", desc = "Mantis" },
+  },
+  config = function()
+    require("mantis").setup({
+      debug = false,
+      hosts = {
+        williamhleucka = {
+          url = "https://mantis.williamhleucka.com",
+          token = "gpYB3wZDDWxm0i0HvMHON2mfMkmq8cSh"
+        },
+        chainlogic = {
+          url = "https://mantis.chainlogic.it",
+          token = "_RNzBd8Y2nDXS2_wR-rghoEfbfJDQ_J2"
+        },
+      }
+    })
+  end
+>>>>>>> 8fc7b3f (fix: update a few things)
 }
