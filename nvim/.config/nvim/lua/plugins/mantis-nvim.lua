8a6fba0a3c0d73212a3ab30dd279d0f604699e77
William Hleucka <william.hleucka@gmail.com>
2026-01-09 07:57:04 -0700

    fix: update dots

--- nvim/.config/nvim/lua/plugins/mantis-nvim.lua
+++ nvim/.config/nvim/lua/plugins/mantis-nvim.lua
@@ -1,27 +1,14 @@
 return {
   enabled = true,
   dir = "/home/whleucka/Projects/mantis-nvim",
+  name = "mantis-nvim",
   event = "VimEnter",
   keys = {
-    { "<leader>M", ":Mantis<cr>", desc = "Mantis" },
+    { "<leader>M", ":Mantis<cr>", desc = "Mantis Issues" },
   },
   dependencies = {
     "grapp-dev/nui-components.nvim",
     "MunifTanjim/nui.nvim"
   },
-  config = function()
-    require("mantis").setup({
-      debug = false,
-      hosts = {
-        williamhleucka = {
-          url = "https://mantis.williamhleucka.com",
-          token = "gpYB3wZDDWxm0i0HvMHON2mfMkmq8cSh"
-        },
-        -- chainlogic = {
-        --   url = "https://mantis.chainlogic.it",
-        --   token = "_RNzBd8Y2nDXS2_wR-rghoEfbfJDQ_J2"
-        -- },
-      }
-    })
-  end
+  opts = require("config.mantis-nvim")
 }
