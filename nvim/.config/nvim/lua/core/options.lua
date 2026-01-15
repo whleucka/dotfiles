bb05e11a04f88f954dd2dfaf423544d0789826a2
William Hleucka <william.hleucka@gmail.com>
2026-01-15 08:10:15 -0700

    fix: remove colorful split

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -1,69 +1,72 @@
 -- General Essentials
-vim.opt.number = true             -- Show line numbers
-vim.opt.relativenumber = true     -- Relative line numbers for fast navigation
-vim.opt.mouse = 'a'               -- Enable mouse support
-vim.opt.clipboard = 'unnamedplus' -- System clipboard access
-vim.opt.swapfile = false          -- No swap files (less clutter)
-vim.opt.backup = false            -- No backups (Git got you)
-vim.opt.undofile = true           -- Persistent undo history
+vim.o.number = true             -- Show line numbers
+vim.o.relativenumber = true     -- Relative line numbers for fast navigation
+vim.o.mouse = 'a'               -- Enable mouse support
+vim.o.clipboard = 'unnamedplus' -- System clipboard access
+vim.o.swapfile = false          -- No swap files (less clutter)
+vim.o.backup = false            -- No backups (Git got you)
+vim.o.undofile = true           -- Persistent undo history
 
 -- Smarter Tabs & Indenting
-vim.opt.expandtab = true   -- Use spaces instead of tabs
-vim.opt.shiftwidth = 4     -- Indent size
-vim.opt.tabstop = 4        -- Display width of a tab
-vim.opt.smartindent = true -- Auto-indent new lines
-vim.opt.autoindent = true  -- Copy indent from current line
+vim.o.expandtab = true   -- Use spaces instead of tabs
+vim.o.shiftwidth = 4     -- Indent size
+vim.o.tabstop = 4        -- Display width of a tab
+vim.o.smartindent = true -- Auto-indent new lines
+vim.o.autoindent = true  -- Copy indent from current line
 
 -- UI/UX Improvements
-vim.opt.cmdheight = 0         -- No command height
-vim.opt.cursorline = true     -- Highlight current line
-vim.opt.termguicolors = true  -- 24-bit color support
-vim.opt.guicursor = "a:"
-vim.opt.signcolumn = 'yes'    -- Always show signcolumn (gutter)
-vim.opt.scrolloff = 8         -- Context lines above/below cursor
-vim.opt.sidescrolloff = 8     -- Context on sides for horizontal movement
-vim.opt.wrap = false          -- Don't wrap long lines
-vim.opt.winborder = "rounded" -- Rounded borders
+vim.o.cmdheight = 0         -- No command height
+vim.o.cursorline = true     -- Highlight current line
+vim.o.termguicolors = true  -- 24-bit color support
+vim.o.guicursor = "a:"
+vim.o.signcolumn = 'yes'    -- Always show signcolumn (gutter)
+vim.o.scrolloff = 8         -- Context lines above/below cursor
+vim.o.sidescrolloff = 8     -- Context on sides for horizontal movement
+vim.o.wrap = false          -- Don't wrap long lines
+vim.o.winborder = "rounded" -- Rounded borders
 vim.cmd("set completeopt=menuone,noinsert,noselect")
 
 -- Searching and Navigation
-vim.opt.ignorecase = true    -- Ignore case when searching
-vim.opt.smartcase = true     -- Case-sensitive if uppercase used
-vim.opt.incsearch = true     -- Search as you type
-vim.opt.hlsearch = true      -- Highlight matches
-vim.opt.inccommand = 'split' -- Live preview of substitute
+vim.o.ignorecase = true    -- Ignore case when searching
+vim.o.smartcase = true     -- Case-sensitive if uppercase used
+vim.o.incsearch = true     -- Search as you type
+vim.o.hlsearch = true      -- Highlight matches
+vim.o.inccommand = 'split' -- Live preview of substitute
 
 -- File and Buffer Management
-vim.opt.splitbelow = true -- Horizontal splits below
-vim.opt.splitright = true -- Vertical splits to the right
-vim.opt.hidden = true     -- Keep buffers open in background
-vim.opt.confirm = true    -- Confirm to save before closing
+vim.o.splitbelow = true -- Horizontal splits below
+vim.o.splitright = true -- Vertical splits to the right
+vim.o.hidden = true     -- Keep buffers open in background
+vim.o.confirm = true    -- Confirm to save before closing
 
 -- Performance
-vim.opt.updatetime = 200 -- Faster CursorHold, good for LSP
-vim.opt.timeoutlen = 300 -- Shorter delay for mapped sequences
+vim.o.updatetime = 200 -- Faster CursorHold, good for LSP
+vim.o.timeoutlen = 300 -- Shorter delay for mapped sequences
 
 -- Aesthetics
-vim.opt.showmode = false                -- Don’t show mode (use statusline plugin)
-vim.opt.laststatus = 3                  -- Global statusline (Neovim 0.7+)
-vim.opt.fillchars:append { eob = ' ' }  -- No ~ at end of buffers
+vim.o.showmode = false                -- Don’t show mode (use statusline plugin)
+vim.o.laststatus = 3                  -- Global statusline (Neovim 0.7+)
+--vim.o.fillchars:append { eob = ' ' }  -- No ~ at end of buffers
 
 -- Extra Ninja Options
-vim.opt.lazyredraw = true           -- Faster macro execution
-vim.opt.virtualedit = 'block'       -- Allow cursor beyond EOL in visual block
-vim.opt.whichwrap:append('<,>,[,]') -- Left/right move across lines
+vim.o.lazyredraw = true           -- Faster macro execution
+vim.o.virtualedit = 'block'       -- Allow cursor beyond EOL in visual block
+--vim.o.whichwrap:append('<,>,[,]') -- Left/right move across lines
 
 -- Folds
-vim.opt.foldmethod = "expr"
-vim.opt.foldlevel = 99
-vim.opt.foldlevelstart = 1
-vim.opt.foldenable = false
+vim.o.foldmethod = "expr"
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 1
+vim.o.foldenable = false
 
 vim.wo.winbar = ""
 
 -- Spelling
-vim.opt.spelllang = 'en_ca'
+vim.o.spelllang = 'en_ca'
+
+-- Win sep
+vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#a6e3a1" })
 
 -- Neovim 0.12+
-vim.opt.diffopt:append('linematch:60') -- Better diffs with linematch algorithm
+--vim.o.diffopt:append('linematch:60') -- Better diffs with linematch algorithm
 vim.loader.enable()                    -- Fast startup via Lua module caching
--- nvim/.config/nvim/lua/plugins/colorful-winsep.lua
+++
@@ -1,4 +0,0 @@
-return {
-  "nvim-zh/colorful-winsep.nvim",
-  event = "WinLeave",
-}
