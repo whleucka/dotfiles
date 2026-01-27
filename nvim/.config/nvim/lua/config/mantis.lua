9b63ecca7d7ffcbeaf87dcc29184d5d948ea062f
William Hleucka <william.hleucka@gmail.com>
2026-01-27 11:13:49 -0700

    fix: mantis config

---
+++ nvim/.config/nvim/lua/config/mantis.lua
@@ -0,0 +1,18 @@
+return {
+  view_issues = {
+    default_filter = "assigned",
+    limit = 250,
+  },
+  hosts = {
+    {
+      name = "williamhleucka.com",
+      url = "https://mantis.williamhleucka.com",
+      env = "MANTIS_WH",
+    },
+    {
+      name = "chainlogic",
+      url = "https://mantis.chainlogic.it",
+      env = "MANTIS_CL",
+    }
+  },
+}
