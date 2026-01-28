be6c7f328d31cc4e1d8596db918e05cb576b0a75
William Hleucka <william.hleucka@gmail.com>
2026-01-28 14:22:31 -0700

    fix: neovim mantis issues, hypr nvim launch

--- nvim/.config/nvim/lua/plugins/mantis.lua
+++ nvim/.config/nvim/lua/plugins/mantis.lua
@@ -5,7 +5,7 @@ return {
   "whleucka/mantis.nvim",
   cmd = { "MantisIssues", "MantisSelectHost" },
   keys = {
-    { "<C-i>", ":MantisIssues<cr>", desc = "Mantis Issues" },
+    { "<leader>M", ":MantisIssues<cr>", desc = "Mantis Issues" },
   },
   dependencies = {
     "nvim-lua/plenary.nvim",
