271826d730d9a93f1c1293d4250c060e21cbfcda
William Hleucka <william.hleucka@gmail.com>
2026-04-09 12:50:11 -0600

    fix: update mini-statusline from default

--- nvim/.config/nvim/lua/plugins/mini-statusline.lua
+++ nvim/.config/nvim/lua/plugins/mini-statusline.lua
@@ -3,5 +3,27 @@ return {
   event = "VimEnter",
   opts = {
     use_icons = true,
+    content = {
+      active = function()
+        local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+        local git = MiniStatusline.section_git({ trunc_width = 40 })
+        local diff = MiniStatusline.section_diff({ trunc_width = 75 })
+        local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+        local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
+        local filename = MiniStatusline.section_filename({ trunc_width = 140 })
+        local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
+        local location = "%l:%v"
+
+        return MiniStatusline.combine_groups({
+          { hl = mode_hl, strings = { mode } },
+          { hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics, lsp } },
+          "%<",
+          { hl = "MiniStatuslineFilename", strings = { filename } },
+          "%=",
+          { hl = "MiniStatuslineFileinfo", strings = { search } },
+          { hl = mode_hl, strings = { location } },
+        })
+      end,
+    },
   },
 }
