46fd2d8c2a4f3c0171b76c20e9c43fd21eaa1785
William Hleucka <william.hleucka@gmail.com>
2026-04-10 08:10:11 -0600

    fix: update intelephense config

--- nvim/.config/nvim/lsp/intelephense.lua
+++ nvim/.config/nvim/lsp/intelephense.lua
@@ -29,4 +29,7 @@ return {
   cmd = { 'intelephense', '--stdio' },
   filetypes = { 'php' },
   root_markers = { '.git', 'composer.json' },
+  init_options = {
+    licenceKey = os.getenv('HOME') .. '/intelephense/license.txt',
+  },
 }
