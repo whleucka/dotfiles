c5cea2a91edd813921842f67828e49c08c3f4572
William Hleucka <william.hleucka@gmail.com>
2026-04-14 09:56:48 -0600

    fix: update reverb

--- nvim/.config/nvim/lua/plugins/reverb.lua
+++ nvim/.config/nvim/lua/plugins/reverb.lua
@@ -1,3 +1,11 @@
+local sound_dir = vim.fn.stdpath("config") .. "/sound_effects/"
 return {
   "https://github.com/whleucka/reverb.nvim",
+  opts = {
+    player = "mpv",
+    sounds = {
+      BufWrite = { path = { sound_dir .. "ding.wav" }, volume = 100 },
+      VimEnter = { path = { sound_dir .. "bell.mp3", volume = 80 } },
+    },
+  },
 }
