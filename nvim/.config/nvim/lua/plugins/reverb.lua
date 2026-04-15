85f947f1e1f03d7f7236b6f22288b9dca7f9f02f
William Hleucka <william.hleucka@gmail.com>
2026-04-15 10:19:43 -0600

    fix: update sound effects

--- nvim/.config/nvim/lua/plugins/reverb.lua
+++ nvim/.config/nvim/lua/plugins/reverb.lua
@@ -1,11 +1,12 @@
 local sound_dir = vim.fn.stdpath("config") .. "/sound_effects/"
 return {
   "https://github.com/whleucka/reverb.nvim",
+  event = "VeryLazy",
   opts = {
-    player = "mpv",
+    player = "paplay",
     sounds = {
-      BufWrite = { path = { sound_dir .. "ding.wav" }, volume = 100 },
-      VimEnter = { path = { sound_dir .. "bell.mp3", volume = 80 } },
+      BufWritePost = { path = { sound_dir .. "completion-success.oga" }, volume = 40 },
+      BufEnter = { path = { sound_dir .. "bell.oga" }, volume = 40 },
     },
   },
 }
