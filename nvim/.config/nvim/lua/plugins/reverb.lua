3158efa23ec6b98e3b2c56751a8274f9ce6be5ce
William Hleucka <william.hleucka@gmail.com>
2026-05-01 13:48:15 -0600

    fix: update reverb config

--- nvim/.config/nvim/lua/plugins/reverb.lua
+++ nvim/.config/nvim/lua/plugins/reverb.lua
@@ -5,13 +5,17 @@ return {
   opts = {
     player = "paplay",
     sounds = {
-      VimEnter = { path = { sound_dir .. "sega-hd.mp3" }, volume = 60 },
-      VimLeavePre = { path = { sound_dir .. "sonic_exit.mp3" }, volume = 60 },
-      BufWritePost = { path = { sound_dir .. "ff8-save.mp3" }, volume = 65 },
-      BufEnter = { path = { sound_dir .. "balloon.mp3" }, volume = 60 },
-      TextYankPost = { path = { sound_dir .. "yoink.mp3" }, volume = 50 },
-      ModeChanged = { path = { sound_dir .. "bell.oga" }, volume = 50 },
-      PackChanged = { path  = { sound_dir .. "1up.mp3" }, volume = 65 },
+      VimEnter = { path = sound_dir .. "sega-hd.mp3", volume = 60 },
+      VimLeavePre = { path = sound_dir .. "sonic_exit.mp3", volume = 60 },
+      BufWritePost = { path = sound_dir .. "ff8-save.mp3", volume = 65 },
+      BufEnter = { path = sound_dir .. "balloon.mp3", volume = 60 },
+      TextYankPost = { path = sound_dir .. "yoink.mp3", volume = 50 },
+      ModeChanged = { path = sound_dir .. "bell.oga", volume = 50 },
+      -- only package updates
+      PackChanged = { path  = sound_dir .. "1up.mp3", volume = 65 , pattern = "update" },
+      User = {
+        { path  = sound_dir .. "coin.mp3", volume = 65, pattern = "FugitiveChanged" },
+      }
     },
   },
 }
