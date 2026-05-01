0a74e28a4cb7cbdb28a1d72de7e0a3b6a04ab1c8
William Hleucka <william.hleucka@gmail.com>
2026-05-01 13:39:47 -0600

    fix: update reverb sounds

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,6 +1,9 @@
 return {
   "lewis6991/gitsigns.nvim",
   event = { "BufReadPost", "VeryLazy" },
+  opts = {
+    current_line_blame = true
+  },
   keys = function()
     return {
       {
--- nvim/.config/nvim/lua/plugins/reverb.lua
+++ nvim/.config/nvim/lua/plugins/reverb.lua
@@ -5,8 +5,13 @@ return {
   opts = {
     player = "paplay",
     sounds = {
-      BufWritePost = { path = { sound_dir .. "completion-success.oga" }, volume = 40 },
-      BufEnter = { path = { sound_dir .. "bell.oga" }, volume = 40 },
+      VimEnter = { path = { sound_dir .. "sega-hd.mp3" }, volume = 60 },
+      VimLeavePre = { path = { sound_dir .. "sonic_exit.mp3" }, volume = 60 },
+      BufWritePost = { path = { sound_dir .. "ff8-save.mp3" }, volume = 65 },
+      BufEnter = { path = { sound_dir .. "balloon.mp3" }, volume = 60 },
+      TextYankPost = { path = { sound_dir .. "yoink.mp3" }, volume = 50 },
+      ModeChanged = { path = { sound_dir .. "bell.oga" }, volume = 50 },
+      PackChanged = { path  = { sound_dir .. "1up.mp3" }, volume = 65 },
     },
   },
 }
