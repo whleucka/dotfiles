6bf8529984716a98582a83373b97d0d65142315a
William Hleucka <william.hleucka@gmail.com>
2026-05-01 14:12:36 -0600

    fix: update some configs

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,4 +1,6 @@
 return {
+
+
   "lewis6991/gitsigns.nvim",
   event = { "BufReadPost", "VeryLazy" },
   opts = {
--- nvim/.config/nvim/lua/plugins/reverb.lua
+++ nvim/.config/nvim/lua/plugins/reverb.lua
@@ -5,8 +5,7 @@ return {
   opts = {
     player = "paplay",
     sounds = {
-      VimEnter = { path = sound_dir .. "sega-hd.mp3", volume = 60 },
-      VimLeavePre = { path = sound_dir .. "sonic_exit.mp3", volume = 60 },
+      VimLeavePre = { path = sound_dir .. "fah.mp3", volume = 60 },
       BufWritePost = { path = sound_dir .. "ff8-save.mp3", volume = 65 },
       BufEnter = { path = sound_dir .. "balloon.mp3", volume = 60 },
       TextYankPost = { path = sound_dir .. "yoink.mp3", volume = 50 },
@@ -15,6 +14,7 @@ return {
       PackChanged = { path  = sound_dir .. "1up.mp3", volume = 65 , pattern = "update" },
       User = {
         { path  = sound_dir .. "coin.mp3", volume = 65, pattern = "NeogitCommitComplete" },
+        { path  = sound_dir .. "flawless.mp3", volume = 65, pattern = "NeogitPushComplete" },
       }
     },
   },
