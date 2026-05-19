c5e331963b5a785a68223295c5331d7f6b749495
William Hleucka <william.hleucka@gmail.com>
2026-05-19 07:15:27 -0600

    fix: disable reverb

--- nvim/.config/nvim/lua/plugins/reverb.lua
+++ nvim/.config/nvim/lua/plugins/reverb.lua
@@ -1,6 +1,7 @@
 local sound_dir = vim.fn.stdpath("config") .. "/sound_effects/"
 return {
   "https://github.com/whleucka/reverb.nvim",
+  enabled = false,
   event = "VeryLazy",
   opts = {
     player = "paplay",
