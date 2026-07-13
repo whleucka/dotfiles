8977af03f9677ee54489cc7e8501c325c8b18f93
William Hleucka <william.hleucka@gmail.com>
2026-07-13 17:05:41 -0600

    feat: clamshell dock

--- hyprland/.config/hypr/keybinds.lua
+++ hyprland/.config/hypr/keybinds.lua
@@ -156,6 +156,13 @@ hl.bind("XF86AudioPrev", hl.dsp.exec_cmd("playerctl previous"))
 hl.bind("XF86AudioStop", hl.dsp.exec_cmd("playerctl stop"))
 
 -- -----------------------------------------------------------------------------
+-- Lid switch (clamshell mode when docked)
+-- -----------------------------------------------------------------------------
+
+hl.bind("switch:on:Lid Switch", hl.dsp.exec_cmd(hypr_scripts .. "/clamshell close"), { locked = true })
+hl.bind("switch:off:Lid Switch", hl.dsp.exec_cmd(hypr_scripts .. "/clamshell open"), { locked = true })
+
+-- -----------------------------------------------------------------------------
 -- Lock
 -- -----------------------------------------------------------------------------
 
