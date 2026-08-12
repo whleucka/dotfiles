f522a58d0800e653439e045e49295369beb358a8
William Hleucka <william.hleucka@gmail.com>
2026-08-12 10:58:25 -0600

    fix: remove hashiru owned configs

--- hyprland/.config/waybar/scripts/cycle-power-profile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-current=$(cat /sys/firmware/acpi/platform_profile)
-case $current in
-    performance) next="balanced" ;;
-    balanced)    next="power-saver" ;;
-    *)           next="performance" ;;
-esac
-sudo tlp $next
